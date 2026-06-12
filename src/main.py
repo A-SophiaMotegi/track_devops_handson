7d50d6ec459eeaef48705806788ee510c5cf1e41
A-SophiaMotegi <as.motegi@tcs.com>
2026-06-12 13:12:47 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 def add(a, b, c=0):
-    try:
-        return int(a + b) + int(c)
-    except:
-        return "error"
-print(add(1.2, 3.5))
+    # 型チェック
+    if not all(isinstance(x, (int, float)) for x in [a, b, c]):
+        return -1
+
+    # 範囲チェック
+    if not (0 <= a <= 10 and 0 <= b <= 10 and 0 <= c <= 10):
+        return -2
+
+    # 正常処理
+    return a + b + c
+    
